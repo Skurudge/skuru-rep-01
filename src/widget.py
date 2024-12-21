a0026f5d5aa267052ff3d788e36d7a649944bd69
Konstantin Bogatyrev <k.grumant@gmail.com>
2024-12-21 21:27:53 +0300

    финальный коммит после проверки перед сдачей домашки 9.2

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,8 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
-    """Принимает на вход строку, содержащую тип и номер карты или счета и возвращает строку с замаскированным номером"""
+    """Принимает на вход строку c типом и номером карты / счета и возвращает строку с замаскированным номером"""
     full_number = account_card.split()[-1]
     masked_number = ""
     if len(full_number) == 16:
@@ -18,4 +18,3 @@ def get_date(complex_format_date: str) -> str:
     """Принимает строку с датой в длинном форме, возвращает строку с датой в формате ДД.ММ.ГГГГ"""
     simple_format_date = complex_format_date[8:10] + "." + complex_format_date[5:7] + "." + complex_format_date[:4]
     return simple_format_date
-
