4d4f63337d3b1cc1dfd23c59917aeb5f0fdd4fcb
Konstantin Bogatyrev <k.grumant@gmail.com>
2024-12-24 22:05:56 +0300

    написана функция get_date со строкой документации. Создан файл README.md

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
-    """Принимает на вход строку c типом и номером карты / счета и возвращает строку с замаскированным номером"""
+    """Принимает на вход строку с типом и номером карты / счета и возвращает строку с замаскированным номером"""
     full_number = account_card.split()[-1]
     masked_number = ""
     if len(full_number) == 16:
@@ -15,6 +15,6 @@ def mask_account_card(account_card: str) -> str:
 
 
 def get_date(complex_format_date: str) -> str:
-    """Принимает строку с датой в длинном форме, возвращает строку с датой в формате ДД.ММ.ГГГГ"""
+    """Принимает строку с датой в длинной форме, возвращает строку с датой в формате ДД.ММ.ГГГГ"""
     simple_format_date = complex_format_date[8:10] + "." + complex_format_date[5:7] + "." + complex_format_date[:4]
     return simple_format_date
