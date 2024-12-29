ea5bd5b3a760375a7fdcf07edfe068ad3770f921
Konstantin Bogatyrev <k.grumant@gmail.com>
2024-12-29 18:40:23 +0300

    подготовлена финальная редакция файла processing.py с двумя функциями: одна фильтрует список словарей по ключу 'state', другая сортирует список словарей по ключу 'date'

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,15 @@
-list_of_dicts = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-
-def filter_by_state(the_list, state_mod='EXECUTED'):
+def filter_by_state(list_of_dicts, state_mod='EXECUTED'):
+    """Функция принимает список словарей и опционально значение для ключа state, возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует опциональному значению"""
     filtered_list = []
-    for item in the_list:
+    for item in list_of_dicts:
         if item.get('state') == state_mod:
             filtered_list.append(item)
         else:
             continue
     return filtered_list
 
-qu = filter_by_state(list_of_dicts)
-glen = len(qu)
-qlist = len(list_of_dicts)
 
-print(qu)
-print(glen)
-print(qlist)
+def sort_by_date(list_of_dicts, decreasing=True):
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки словарей по дате (по умолчанию - убывание), возвращает новый список словарей, отсортированный по дате"""
+    sorted_list = sorted(list_of_dicts, key=lambda x: x['date'], reverse=decreasing)
+    return sorted_list
