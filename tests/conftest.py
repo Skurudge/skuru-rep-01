83306db974d27827972ea1faf1398bcb6d6dad77
Konstantin Bogatyrev <k.grumant@gmail.com>
2025-01-02 22:19:29 +0300

    разработан модуль tests/test_widget.py для тестирования функций из модуля src/widget.py, внесены сопутствующие изменения

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,18 @@ def card_number_example() -> int:
 @pytest.fixture
 def account_example() -> int:
     return 45236587452365845987
+
+
+@pytest.fixture
+def card_type_example() -> str:
+    return "Visa Platinum 7000792289606361"
+
+
+@pytest.fixture
+def account_type_example() -> str:
+    return "Счет 73654108430135874305"
+
+
+@pytest.fixture
+def date_example() -> str:
+    return "2024-03-11T02:26:18.671407"
